baa1574d78fccdc1015159566e407ad9fa4e5f15
Susan Cheng <susan.doggie@gmail.com>
2021-07-27 00:05:07 +0800

    clean

--- Sources/DBBrowser/js/pages/Home/index.js
+++ Sources/DBBrowser/js/pages/Home/index.js
@@ -18,6 +18,8 @@ import eventEmitter from '../../utils/eventEmitter';
 
 import { withDatabase } from '../../utils/database';
 
+import _encode_data from '../../components/DataSheet/_encode_data';
+
 class Home extends React.Component {
 
   constructor(props) {
@@ -288,7 +290,11 @@ class Home extends React.Component {
 
     if (_.isArray(this.state.result) && this.state.resultStyle == 'table')  {
 
+      const columns = this.state.result.reduce((result, x) => _.uniq(result.concat(Object.keys(x))), []);
+      const grid = this.state.result.map(x => columns.map(c => _encode_data(x[c])));
 
+      // const blob = new Blob([file], { type: 'text/csv' });
+      // saveAs(blob, _.isEmpty(this.state.currentTable) ? 'file.csv' : `${this.state.currentTable}.csv`);
 
     } else {
 
